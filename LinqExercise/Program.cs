c02d5096f02f4c3402dfb01133e4c0d2e5bd5556
Cruz Sanchez <csanchez@truecoders.io>
2020-02-04 08:28:44 -0600

    Ready for students

--- LinqExercise/Program.cs
+++ LinqExercise/Program.cs
@@ -6,35 +6,51 @@ namespace LinqExercise
 {
     class Program
     {
+        //Static array of integers
         private static int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 
         static void Main(string[] args)
         {
-            // List of employees
-            var employees = CreateEmployees();
+            /*
+             * 
+             * Complete every task using Method OR Query syntax. 
+             * You may find that Method syntax is easier to use since it is most like C#
+             * Every one of these can be completed using Linq and then printing with a foreach loop.
+             * Push to your github when completed!
+             * 
+             */
 
-            //Print all the employees' FullName properties to the console only if their first name starts with C OR S.
-            //Order this in acesnding order by FirstName.
-
-            //Print all the employees' FullName and Age who are over the age 26 to the console.
-            //Order this by Age first and then by FirstName in the same result.
-
-            //Print the Sum and Average of the employees YearsOfExperience if their YOE is less than or equal to 10 AND Age is greater than 35
-
-            //Add an employee to the end of the list without using employees.Add()
-
-            //Print the Sum and average of numbers
+            //Print the Sum and Average of numbers
 
             //Order numbers in ascending order and decsending order. Print each to console.
 
             //Print to the console only the numbers greater than 6
 
-            //Order numbers in any order but only print 4 **foreach loop only!**
+            //Order numbers in any order (acsending or desc) but only print 4 of them **foreach loop only!**
+
+            //Change the value at index 4 to your age, then print the numbers in decsending order
+
+            // List of employees ***Do not remove this***
+            var employees = CreateEmployees();
+
+            //Print all the employees' FullName properties to the console only if their FirstName starts with a C OR an S.
+            //Order this in acesnding order by FirstName.
+
+            //Print all the employees' FullName and Age who are over the age 26 to the console.
+            //Order this by Age first and then by FirstName in the same result.
+
+            //Print the Sum and then the Average of the employees' YearsOfExperience
+            //if their YOE is less than or equal to 10 AND Age is greater than 35
+
+            //Add an employee to the end of the list without using employees.Add()
+
+            
             Console.WriteLine();
 
             Console.ReadLine();
         }
 
+        #region CreateEmployeesMethod
         private static List<Employee> CreateEmployees()
         {
             List<Employee> employees = new List<Employee>();
@@ -51,5 +67,6 @@ namespace LinqExercise
 
             return employees;
         }
+        #endregion
     }
 }
