974d9b60f58dd7a32b6ce49bf4a81563dd5fded3
Michael Doyle <mdoyle@truecoders.io>
2020-07-10 10:21:51 -0500

    Update Employee.cs
    added default constructor to Employee class

--- LinqExercise/Employee.cs
+++ LinqExercise/Employee.cs
@@ -25,5 +25,10 @@ namespace LinqExercise
             Age = age;
             YearsOfExperience = yearsOfExperience;
         }
+        
+        public Employee()
+        {
+            
+        }
     }
 }
